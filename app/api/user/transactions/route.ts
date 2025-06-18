bcab3676385f8e198665561c8995d54766cb9b1f
ooovenenoso <120500656+ooovenenoso@users.noreply.github.com>
2025-06-18 09:02:12 -0400

    fix transactions endpoint auth

--- app/api/user/transactions/route.ts
+++ app/api/user/transactions/route.ts
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server"
-import { createClient } from "@/lib/supabase"
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers"
 
 export async function GET(request: Request) {
   try {
-    const supabase = createClient()
+    const supabase = createRouteHandlerClient({ cookies })
 
     // Get the current user
     const {
